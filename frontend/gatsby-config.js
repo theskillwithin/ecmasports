20015712ae54d5466d0834c1bb181dee0db4dd82
theskillwithin <austinppeterson@gmail.com>
2020-03-07 13:03:55 -0700

    s3 deploy

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: ".env",
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -25,6 +29,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "ecmasports",
+        acl: null,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
