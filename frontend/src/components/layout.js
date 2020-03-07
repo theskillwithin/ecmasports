c1b3cea58544c12b7d00b7c7ac7954e9d25cf25a
theskillwithin <austinppeterson@gmail.com>
2020-03-07 14:51:12 -0700

    normalize.css

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import "normalize.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -34,7 +35,12 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
+        <footer
+          style={{
+            margin: `5rem auto 0`,
+            textAlign: "center",
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,19 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "components/layout"
-import Image from "components/image"
+import logo from "images/Heg.svg"
 import SEO from "components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hello World</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="ECMASports" />
+    <center>
+      <img src={logo} alt=" " />
+    </center>
   </Layout>
 )
 
